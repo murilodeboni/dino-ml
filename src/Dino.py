60e3505026eb11c06a68561aae1f88037dfae099
Murilo Deboni <murilo.deboni@gmail.com>
2025-06-06 10:43:58 +0200

    python dino game

---
+++ src/Dino.py
@@ -0,0 +1,43 @@
+import pygame
+from src.constants import *
+
+
+class Dino:
+    def __init__(self):
+        self.x = 50
+        self.y = GROUND_Y - DINO_HEIGHT
+        self.width = DINO_WIDTH
+        self.height = DINO_HEIGHT
+        self.velocity_y = 0
+        self.is_jumping = False
+        self.is_ducking = False
+
+    def jump(self):
+        if not self.is_jumping:
+            self.velocity_y = JUMP_VELOCITY
+            self.is_jumping = True
+
+    def duck(self):
+        if not self.is_jumping:
+            self.is_ducking = True
+            self.height = DINO_HEIGHT // 2
+            self.y = GROUND_Y - self.height
+
+    def unduck(self):
+        if self.is_ducking:
+            self.is_ducking = False
+            self.height = DINO_HEIGHT
+            self.y = GROUND_Y - self.height
+
+    def update(self):
+        self.y += self.velocity_y
+        if self.is_jumping:
+            self.velocity_y += GRAVITY
+        if self.y >= GROUND_Y - self.height:
+            self.y = GROUND_Y - self.height
+            self.velocity_y = 0
+            self.is_jumping = False
+
+    def draw(self, screen):
+        color = (100, 100, 100)
+        pygame.draw.rect(screen, color, (self.x, self.y, self.width, self.height))
---
+++ src/Obstacle.py
@@ -0,0 +1,30 @@
+import pygame
+from src.constants import *
+import random
+
+
+class Obstacle:
+    def __init__(self, x, kind):
+        self.x = x
+        self.kind = kind  # "tree" or "bird"
+        if kind == "tree":
+            self.y = GROUND_Y - OBSTACLE_HEIGHT
+            self.width = OBSTACLE_WIDTH
+            self.height = OBSTACLE_HEIGHT
+        else:  # bird
+            self.y = GROUND_Y - BIRD_HEIGHT - random.choice([50, 80])
+            self.width = OBSTACLE_WIDTH
+            self.height = BIRD_HEIGHT
+
+    def update(self, speed):
+        self.x -= speed
+
+    def draw(self, screen):
+        if self.kind == "tree":
+            color = (34, 177, 76)
+        else:
+            color = (0, 162, 232)
+        pygame.draw.rect(screen, color, (self.x, self.y, self.width, self.height))
+
+    def is_off_screen(self):
+        return self.x + self.width < 0
